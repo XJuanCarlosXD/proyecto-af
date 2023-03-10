ea80d72d5f5d7fd9a0bac6bc121d432db1edf849
XJuanCarlosXD <juancarlos192003@gmail.com>
2023-03-10 10:59:15 -0400

    master

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import router from './Routes/router.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
